9deaf77893a5fa83011037cc516189e1f461abd1
pbywater <philippabywater@gmail.com>
2017-06-05 15:35:16 +0100

    creating react native app

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>HI AKIN</Text>
+        <Text>HI LUCY</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.old.md
@@ -0,0 +1,2 @@
+# imprint
+Portfolio Management
