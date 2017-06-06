f6458139c81e68cbb01e3425c21213afc19cfe35
Akin909 <akin.sowemimo@gmail.com>
2017-06-06 12:05:26 +0100

    Add initial scaffold for portfolio page
    with get photos function

--- components/Appointments.js
+++ components/Appointments.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import styled from 'styled-components/native';
+import { TabNavigator } from 'react-navigation';
+import Portfolio from './Portfolio.js';
 
 class Appointments extends Component {
   render() {
     return (
-      <Text>
-        Appointments
-      </Text>
+      <View>
+        <Text>
+          Appointments
+        </Text>
+      </View>
     );
   }
 }
