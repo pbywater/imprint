305ac0015aa4593ef420f9170acd8142ad8f32f6
PiotrBerebecki <petebere@gmail.com>
2017-06-06 11:08:43 +0100

    Add TabNavigator
    relates #5

---
+++ components/Appointments.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+class Appointments extends Component {
+  render() {
+    return (
+      <Text>
+        Appointments
+      </Text>
+    );
+  }
+}
+
+export default Appointments;
---
+++ components/Portfolio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+class Portfolio extends Component {
+  render() {
+    return (
+      <Text>
+        Portfolio
+      </Text>
+    );
+  }
+}
+
+export default Portfolio;
